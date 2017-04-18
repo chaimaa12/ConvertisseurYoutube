4ad071c535295e450626e312cfc1bce068b7ca43
admin <chaimaa.souri@hotmail.fr>
2017-04-18 14:11:50 +0200

    [IMG/DESIGN] Second Page (2)

--- app/src/main/java/com/example/admin/convertisseuryoutube/ConversionActivity.java
+++ app/src/main/java/com/example/admin/convertisseuryoutube/ConversionActivity.java
@@ -2,17 +2,37 @@ package com.example.admin.convertisseuryoutube;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 /**
  * Created by admin on 17/04/2017.
  */
 
 public class ConversionActivity extends Activity {
+
+    private Button btnRetour;
+    private Button btnDownload;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conversion_layout);
+        btnRetour = (Button) findViewById(R.id.btnBack);
+        btnDownload = (Button) findViewById(R.id.btnDownload);
+
+        btnRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+
 
 
     }
+
+
 }
