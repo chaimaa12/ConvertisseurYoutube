f8ac23c7ef7b6dac579c03be28b5c97e3b130f6a
admin <chaimaa.souri@hotmail.fr>
2017-04-18 09:55:23 +0200

    commit main_layout

---
+++ app/src/main/java/com/example/admin/convertisseuryoutube/ConversionActivity.java
@@ -0,0 +1,18 @@
+package com.example.admin.convertisseuryoutube;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 17/04/2017.
+ */
+
+public class ConversionActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conversion_layout);
+
+
+    }
+}
--- app/src/main/java/com/example/admin/convertisseuryoutube/MainActivity.java
+++ app/src/main/java/com/example/admin/convertisseuryoutube/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.admin.convertisseuryoutube;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+
+
+    private ImageButton convertButton;
+    private EditText editUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editUrl = (EditText) findViewById(R.id.editUrl);
+        convertButton = (ImageButton) findViewById(R.id.ImageButtonConvert);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!editUrl.getText().toString().equals("")){
+
+                    Intent myIntent = new Intent(getApplicationContext(), ConversionActivity.class);
+                    startActivity(myIntent);
+                   // convertUrl(editUrl.getText().toString());
+
+                }
+            }
+        });
     }
+
+
 }
+
